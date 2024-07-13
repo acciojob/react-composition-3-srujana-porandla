ae71217584731aa86d3d691f3ca4e73a9bdbea35
Srujana <p.srujana1356@gmail.com>
2024-07-13 16:23:22 +0530

    tooitip

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   return (
     <div className="App">
-       <h2 className="tooltip">
+       <h2 className="tooltip-heading">
            <Tooltip text="This is a tooltip">
               Hover over me
             </Tooltip>
@@ -22,7 +22,7 @@ const App = () => {
        <br />
        <br />
        <br />
-       <p className="tooltip">
+       <p className="tooltip-para">
         <Tooltip text="This is another tooltip">
            Hover over me to see another tooltip
         </Tooltip> 
